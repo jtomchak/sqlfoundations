9e06e8ff643bcab4143ad77ef7e2e27afcbfd45c
Jesse Tomchak <jtomchak@gmail.com>
2017-06-29 06:11:40 -0700

    oh boy fetching data !!!!

--- orm-app/routes/users.js
+++ orm-app/routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var Appusers = require("../models/appuser");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  Appusers.find(function(err, users) {
+    if (err) return next(err);
+    res.render("users", {
+      users: users
+    });
+  });
 });
 
 module.exports = router;
